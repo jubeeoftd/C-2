5e5d080197b131fdd52e43aee797b870c7d08a30
chris miller <jubeeofsc@gmail.com>
2019-08-05 10:10:47 -0500

    Lab2

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Setting Variables
+            string strPlayerName;
+            int nHits, nAtBats;
+            double dblBattingAverage;
+
+            // prompt for name and recieve
+            Console.WriteLine("~ This Program calculates a Baseball player's batting average ~");
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("Enter Player's Name:");
+            strPlayerName = Console.ReadLine();
+
+            // prompt for hits
+            Console.WriteLine("Enter Player's # of Hits: ");
+            nHits = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
